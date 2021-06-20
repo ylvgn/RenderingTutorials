7ec6437de8ada5172f808e3f594e70022fb80196
gyh <gyhguanyonghua@163.com>
2021-06-21 00:25:12 +0800

    init cmake

---
+++ Learn-OpenGL-programming/README.md
@@ -0,0 +1,5 @@
+## Learn-OpenGL-programming
+
+### 3rdlibs
+* [GLFW](https://www.glfw.org/download.html)
+* [GLAD](https://github.com/Dav1dde/glad)
---
+++ Learn-OpenGL-programming/tutorial01_create_empty_window/src/tutorial01_main.cpp
@@ -0,0 +1,31 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
